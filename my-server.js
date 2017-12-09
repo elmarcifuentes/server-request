d6241ba348a8de11ac90f462ecb2da6baa0c23f7
elmarc <elmarc@dhcp-v117-089.mobile.uci.edu>
2017-12-09 11:21:06 -0800

    added 2nd web server

--- my-server.js
+++ my-server.js
@@ -14,14 +14,26 @@
 
 var http = require("http");
 
-var PORT = 7000;
+var PORT1 = 7000;
+var PORT2 = 7500;
 
-function handleRequest (req, resp) {
+
+function goodRequest (req, resp) {
     resp.end("Way to go! " + req.url);
 };
 
-var server = http.createServer(handleRequest);
+var server = http.createServer(goodRequest);
 
-server.listen(PORT,function() {
-    console.log("listening on: http://localhost:" + PORT )
+server.listen(PORT1,function() {
+    console.log("listening on: http://localhost:" + PORT1 )
+});
+
+function badRequest (req, resp) {
+    resp.end("you suck! " + req.url);
+};
+
+var badServer = http.createServer(badRequest);
+
+badServer.listen(PORT2,function() {
+    console.log("listening on: http://localhost:" + PORT2 )
 });
